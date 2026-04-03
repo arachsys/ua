6ac0834f90fbf5e1883d784ac4272ca48ff3aa1d
Chris Webb <chris@arachsys.com>
2026-04-03 08:39:24 +0100

    Disable internal bash if SANDBOX is unset
    Unwrapped commands can enabled by setting SANDBOX to the empty string.
    
    Add 'ua chat' to examples/ua.bash, running ua with the bash tool disabled
    to chat with the model.

--- examples/ua.bash
+++ examples/ua.bash
@@ -2,17 +2,22 @@ ua() (
   export API=${API:-https://openrouter.ai/api/v1/responses}
   export MODEL=${MODEL:-anthropic/claude-opus-4.6}
 
-  if [[ -z $KEY ]] && [[ -f ~/.config/secrets/openrouter ]]; then
+  if [[ ! -v KEY ]] && [[ -f ~/.config/secrets/openrouter ]]; then
     export KEY=$(< ~/.config/secrets/openrouter)
   fi
 
-  if [[ -z $SANDBOX ]] && [[ -x ~/.config/ua/sandbox ]]; then
+  if [[ ! -v SANDBOX ]] && [[ -x ~/.config/ua/sandbox ]]; then
     export SANDBOX=~/.config/ua/sandbox
   fi
 
   if [[ ! -f ${LOG:-ua.log} ]] && [[ -f ~/.config/ua/system ]]; then
-    ua system < ~/.config/ua/system
+    command ua system < ~/.config/ua/system
   fi
 
-  exec ua "$@"
+  if [[ $* == chat ]]; then
+    export -n SANDBOX
+    exec ua
+  else
+    exec ua "$@"
+  fi
 )
